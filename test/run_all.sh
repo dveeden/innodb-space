eb984f007e93ff1305cd25a9af4914ecae2c4514
Daniël van Eeden <git@myname.nl>
2013-11-15 11:50:49 +0100

    Update test scripts

--- test/run_all.sh
+++ test/run_all.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+pushd test > /dev/null 2>&1
 
 python test_check_datafiles.py
+[ $? -ne 0 ] && exit 1
+
 python test_size_to_int.py
+[ $? -ne 0 ] && exit 1
+
+popd test > /dev/null 2>&1
